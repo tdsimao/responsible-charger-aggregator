e844b7a0b213df7a774c2b6b1c733c09b2ff0d4a
tdsimao <tdsimao@gmail.com>
2017-10-18 15:40:44 +0200

    refactoring

--- grid.py
+++ grid.py
@@ -9,9 +9,7 @@ class Grid(object):
         self.n_lines = 3
         self.x = self.initialize_x()
         self.lines = self.init_lines()
-        M = self.compute_m()
-        self.S = self.compute_s()
-        self.Z = self.compute_z()
+        self.S = self._compute_ptdfs()
 
     def init_lines(self):
         return [(0, 1), (0, 2), (1, 2), (1, 0), (2, 0), (2, 1)]
@@ -26,40 +24,44 @@ class Grid(object):
         x[2][1] = x[1][2]
         return x
 
-    def compute_s(self):
-        Z = self.compute_z()
-        S = np.zeros([self.n_nodes, self.n_nodes, self.n_nodes])
+    def _compute_ptdfs(self):
+        """
+        Precomputing Power Transfer Distribution Factors
+        """
+        z = self._compute_z()
+        s = np.zeros([self.n_nodes, self.n_nodes, self.n_nodes])
         for i in range(self.n_nodes):
             for l in range(self.n_nodes):
                 for k in range(self.n_nodes):
                     if k == 0 and l != 0:
-                        S[k, l, i] = -1 * Z[l-1, i-1]
+                        s[k, l, i] = -1 * z[l-1, i-1]
                     elif k != 0 and l == 0:
-                        S[k, l, i] = Z[k-1, i-1]
+                        s[k, l, i] = z[k-1, i-1]
                     elif k != 0 and l != 0 and k != l:
-                        S[k, l, i] = Z[k-1, i-1] - Z[l-1, i-1]
-        return S
+                        s[k, l, i] = z[k-1, i-1] - z[l-1, i-1]
+        return s
 
-    def compute_z(self):
-        M = self.compute_m()
-        return np.linalg.inv(M[1::, 1::])
+    def _compute_z(self):
+        m = self._compute_m()
+        return np.linalg.inv(m[1::, 1::])
 
-    def compute_m(self):
-        M = np.zeros([self.n_nodes,self.n_nodes])
+    def _compute_m(self):
+        m = np.zeros([self.n_nodes,self.n_nodes])
         for i in range(self.n_nodes):
             for j in range(self.n_nodes):
                 if i != j:
-                    M[i, j] = -1./self.x[i, j]
+                    m[i, j] = -1./self.x[i, j]
                 else:
-                    M[i, j] = sum(1./self.x[k, j] for k in range(self.n_nodes) if k != j)
-        return M
+                    m[i, j] = sum(1./self.x[k, j] for k in range(self.n_nodes) if k != j)
+        return m
 
     def compute_flow(self, loads):
         """
-
+        use precomputed ptdfs to calculate the flow in the grid
         :param loads: vector of load in each node
-        :return:
+        :return: flow of the grid given the loads in each node
         """
+        assert len(loads) == self.n_nodes
         flow = np.zeros([self.n_nodes, self.n_nodes])
         for k, l in self.lines:
             flow[k, l] = sum(loads[i] * 1./self.x[k, l] * self.S[k, l, i] for i in range(self.n_nodes-1))
